07b3b8d55dab780d9e72392d8ee6a3a0a6ead287
egekulahli <egekulahli@gmail.com>
2023-07-07 20:44:16 +0300

    initial creation

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a320c6c844msh209be6f8f92db2bp192c4ejsn60b9a9402b1b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = "f020ac85a31eca1211b83a8297fd7a29";
